af252178a38e147945de8af8b54510070d3a1fe6
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-07 16:37:39 -0400

    added collider handler

---
+++ Assets/Scripts/ColliderHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderHandler : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Triggered by " + collider.gameObject.name);
+
+        ParticleSystem explosion = transform.Find("Explosion").GetComponent<ParticleSystem>();
+        explosion.Play();
+        Debug.Log(explosion.name);
+    }
+
+
+}
